18b4d49f477751091fd87ec49c57d233d24dcf8b
Cristian <cristian.c.lopez.m@gmail.com>
2023-09-23 09:13:16 -0500

    fetch con funciones asincronas

--- fetch/js/fetch.js
+++ fetch/js/fetch.js
@@ -16,7 +16,49 @@
         $fetch.appendChild($fragment);
 
     }) 
-    .catch( err => console.log("Error"))
-    .finally(() => console.log("Se ejecutara de cualquier forma"));
+    // .catch( err => console.log("Error"))
+    // .finally(() => console.log("Se ejecutara de cualquier forma"));
 
-})()
+})();
+
+
+
+(() => {
+    const $async = document.getElementById("async"),
+        $fragment = document.createDocumentFragment();
+
+    async function getRequest() {
+        try{
+            /** 
+             * Con el await de la función async podemos controlar el flujo de nuestra petición de 
+             * una forma mucha más ordenata.
+             * TRY - CATCH -> es una forma más simple de manejar nuestras validaciones ya que podemos 
+             * imprimir mensjes mucho más personalizados
+             *  */
+            let response = await fetch("https://jsonplaceholder.typicode.com/users"),
+            json = await response.json();
+
+            if(!response.ok) throw ({status: response.status, statusText: response.statusText})
+
+            json.forEach(element => {
+                const $li = document.createElement("li");
+                $li.innerHTML = `${element.name} -- ${element.email} -- ${element.phone}`;
+                $fragment.appendChild($li);
+            });
+
+            $async.appendChild($fragment);
+
+
+        } catch(error) {
+            console.error(error)
+            let message = error.statusText || "Ocurrió un error";
+            $async.innerHTML =  `Error ${error.status}: ${message}`;
+        } finally{
+            console.log("Codigo que se ejecuta si o si")
+        }
+
+       
+    }
+
+    getRequest();
+})();
