768c5e2f9495722ebd422ce65f37d4fd2079a625
Cristian <cristian.c.lopez.m@gmail.com>
2023-09-23 12:16:33 -0500

    plantilla para hacer el crud con el api rest

---
+++ practica/js/ajax.js
@@ -0,0 +1,6 @@
+const d = document
+const $form = document.querySelector(".crud-form"),
+  $table = document.querySelector(".crud-table"),
+  $tittle = document.querySelector(".crud-tittle"),
+  $template = document.querySelector(".crud-template").content,
+  $fragment = document.createDocumentFragment();
